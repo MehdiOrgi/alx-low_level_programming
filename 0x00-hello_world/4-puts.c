486498daaa056cf0162a8dac554f3e35200816cb
Mehdi Orgi <mehdiorgi@protonmail.com>
2023-02-16 18:17:58 -0800

    File added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
- * main - main function
- *
- * Description:Prints a string to the standard output
- *
- * Return:0(Success)
+ * main - mai function
+ * Return: 0 (success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle,\n");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
