57a0d3107a07a57c7804366bb8bb54a9532df09b
Mehdi Orgi <mehdiorgi@protonmail.com>
2023-02-17 18:05:54 -0800

    File added

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,18 +8,18 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-        int num = 0;
+    int num = 0;
 
-        while (num < 10)
-        {
-                putchar(num + '0');
-                num++;
-        }
+    while (num < 10)
+    {
+        putchar(num + '0');
+        num++;
+    }
 
-        putchar('\n');
+    putchar('\n');
 
-        return (0);
+    return (0);
 }
+
