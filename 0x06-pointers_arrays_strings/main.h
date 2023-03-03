6ea06549ef2ab64724073d287df999f4ba4c1bbc
Mehdi Orgi <mehdiorgi@protonmail.com>
2023-03-02 17:53:09 -0800

    Files uploaded

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,38 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* _putchar - writes the character c to stdout */
 int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
 
-/* 0. memset */
-void *_memset(void *s, int c, size_t n);
-
-/* 1. memcpy */
-void *_memcpy(void *dest, const void *src, size_t n);
-
-/* 2. strchr */
-char *_strchr(char *s, char c);
-
-/* 3. strspn */
-unsigned int _strspn(char *s, char *accept);
-
-/* 4. strpbrk */
-char *_strpbrk(char *s, char *accept);
-
-/* 5. strstr */
-char *_strstr(char *haystack, char *needle);
-
-/* 6. Chess is mental torture */
-int print_chessboard(char (*a)[8]);
-
-/* 7. The line of life is a ragged diagonal between duty and desire */
-char *leet(char *str);
-
-/* 8. Double pointer, double fun */
-void print_diagsums(int *a, int size);
-
-/* 9. My primary goal of hacking was the intellectual curiosity, the seduction of adventure */
-void set_string(char **s, char *to);
-
-#endif /* MAIN_H */
-
+#endif
