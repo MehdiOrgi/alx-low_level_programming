3b81b23209eeb1fb56fab049c60c3b4c6f19cddc
Mehdi Orgi <mehdiorgi@protonmail.com>
2023-03-06 18:02:48 -0800

    Files commited

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring.
+ * @haystack: string to search in.
+ * @needle: substring to search for.
+ *
+ * Return: Pointer to the beginning of the located substring,
+ *         or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		for (j = 0; needle[j]; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+
+		if (!needle[j])
+			return (haystack + i);
+	}
+
+	return (NULL);
+}
