d637d6bfada9313b18475d11092c202c9e883f42
Mehdi Orgi <mehdiorgi@protonmail.com>
2023-03-06 18:13:42 -0800

    Files commited

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,23 +2,24 @@
 
 /**
  * _strchr - locates a character in a string
+ * @s: the string to search
+ * @c: the character to locate
  *
- * @s: pointer to string to search
- * @c: character to find in string
- *
- * Return: pointer to first occurrence of character c in string s,
- *         or NULL if character is not found
+ * Return: a pointer to the first occurrence of the character c in the string s,
+ * or NULL if the character is not found
  */
-
 char *_strchr(char *s, char c)
 {
-	while (*s)
+	while (*s != '\0')
 	{
 		if (*s == c)
 			return (s);
 		s++;
 	}
+
 	if (*s == c)
 		return (s);
+
 	return (NULL);
 }
+
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,32 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* _putchar - writes the character c to stdout */
+
+#include <stdio.h>
+#include <stdlib.h>
 int _putchar(char c);
-
-/* 0. memset */
 char *_memset(char *s, char b, unsigned int n);
-
-/* 1. memcpy */
 char *_memcpy(char *dest, char *src, unsigned int n);
-
-/* 2. strchr */
 char *_strchr(char *s, char c);
-
-/* 3. strspn */
 unsigned int _strspn(char *s, char *accept);
-
-/* 4. strpbrk */
 char *_strpbrk(char *s, char *accept);
-
-/* 5. strstr */
 char *_strstr(char *haystack, char *needle);
-
-/* 6. Chess is mental torture */
 void print_chessboard(char (*a)[8]);
-
-/* 7. The line of life is a ragged diagonal between duty and desire */
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
-#endif /* MAIN_H */
-
+#endif/*MAIN_H*/
