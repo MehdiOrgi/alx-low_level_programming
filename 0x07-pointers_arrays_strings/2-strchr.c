ab1d79ed02c5af661aa027ec69bf308316e9b767
Mehdi Orgi <mehdiorgi@protonmail.com>
2023-03-06 18:05:47 -0800

    Files commited

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * _strchr - locates a character in a string
- * @s: the string to search
- * @c: the character to locate
+ * _strchr - locate a character in a string
+ * @s: string to search
+ * @c: character to find
  *
  * Return: pointer to the first occurrence of the character c in the string s,
- * or NULL if the character is not found
+ *         or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
-	while (*s)
+	while (*s != '\0')
 	{
 		if (*s == c)
 			return (s);
 		s++;
 	}
-	if (c == '\0')
+	if (*s == c)
 		return (s);
 	return (NULL);
 }
