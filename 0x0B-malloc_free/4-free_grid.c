86906e6f497a3b64fa21f7d46dd0e8b5f3d4045c
Mehdi Orgi <mehdiorgi@protonmail.com>
2023-03-14 18:50:01 -0700

    Files commited

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- * free_grid - frees a 2D grid previously created by alloc_grid
- * @grid: pointer to the 2D grid
+ * free_grid - frees a 2 dimensional grid previously created
+ * @grid: pointer to the grid
  * @height: height of the grid
  *
  * Return: void
  */
 void free_grid(int **grid, int height)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < height; i++)
-    {
-        free(grid[i]);
-    }
+	if (grid == NULL || height <= 0)
+		return;
 
-    free(grid);
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
 }
 
