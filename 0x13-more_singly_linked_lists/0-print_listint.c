ac0d13a2351a3fd75e05da5df786fe2fa0461f80
Mehdi Orgi <mehdiorgi@protonmail.com>
2023-04-04 19:42:33 -0700

    Files commited

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,13 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_listint - Prints all the elements of a listint_t list.
+ * print_listint - prints all the elements of a listint_t list.
  *
- * @h: Head of linklist node.
+ * @h: head of linklist node
  *
- * Return: The number of nodes.
+ * Return: the number of nodes
+ *
  */
 size_t print_listint(const listint_t *h)
 {
@@ -17,7 +19,5 @@ size_t print_listint(const listint_t *h)
 		h = h->next;
 		count++;
 	}
-
 	return (count);
 }
-
