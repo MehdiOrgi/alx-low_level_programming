6cb8fd4c79aaf2f42de16111579b275f4b4e8f39
Mehdi <Mehdiorgi@protonmail.com>
2023-10-30 18:41:55 -0700

    Files commited

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,13 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-#include <fcntl.h>  // Include the necessary header for open()
 
 /**
- * error_file - checks if files can be opened.
- * @file_from: file descriptor for the source file.
- * @file_to: file descriptor for the destination file.
- * @argv: arguments vector.
- * Return: no return.
+ * error_file - Check if files can be opened.
+ * @file_from: File descriptor for the source file.
+ * @file_to: File descriptor for the destination file.
+ * @argv: Arguments vector.
+ * Return: No return value.
  */
 void error_file(int file_from, int file_to, char *argv[])
 {
@@ -24,10 +23,10 @@ void error_file(int file_from, int file_to, char *argv[])
 }
 
 /**
- * main - check the code for Holberton School students.
- * @argc: number of arguments.
- * @argv: arguments vector.
- * Return: Always 0.
+ * main - Program entry point for copying files.
+ * @argc: Number of arguments.
+ * @argv: Arguments vector.
+ * Return: Always returns 0.
  */
 int main(int argc, char *argv[])
 {
@@ -37,23 +36,24 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
-	while ((nchars = read(file_from, buf, 1024)) > 0)
+	nchars = 1024;
+	while (nchars == 1024)
 	{
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
 		nwr = write(file_to, buf, nchars);
 		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
-
-	if (nchars == -1)
-		error_file(-1, 0, argv);
 
 	err_close = close(file_from);
 	if (err_close == -1)
@@ -65,7 +65,7 @@ int main(int argc, char *argv[])
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to); // Change file_from to file_to here
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
