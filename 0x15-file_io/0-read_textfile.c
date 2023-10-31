7a6d2ca2ca5da2578511875a4d06a1ad8bea080d
Mehdi <Mehdiorgi@protonmail.com>
2023-10-30 18:36:14 -0700

    files uploaded

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,51 +1,37 @@
 #include "main.h"
 
 /**
- * read_textfile - Read and print a text file to standard output
- * @filename: The name of the file to read
- * @letters: The number of letters to read and print
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
  *
- * Return: The actual number of letters read and printed. If an error occurs,
- * or if the file can't be opened or read, return 0.
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t num_read, num_written;
-    char *buf;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-    if (filename == NULL)
-        return (0);
+	if (!filename)
+		return (0);
 
-    buf = malloc(sizeof(char) * letters);
-    if (buf == NULL)
-        return (0);
+	fd = open(filename, O_RDONLY);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    {
-        free(buf);
-        return (0);
-    }
+	if (fd == -1)
+		return (0);
 
-    num_read = read(fd, buf, letters);
-    if (num_read == -1)
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-    num_written = write(STDOUT_FILENO, buf, num_read);
-    if (num_written == -1 || num_read != num_written)
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-    free(buf);
-    close(fd);
-    return (num_written);
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
 }
 
